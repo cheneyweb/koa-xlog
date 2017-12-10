88d369727760116ec1bb717728364f8dc3106103
cheneyweb <457299596@qq.com>
2017-12-10 22:47:36 +0800

    精简内部配置

--- xlog_modules/koa-xlog/index.js
+++ xlog_modules/koa-xlog/index.js
@@ -1,14 +1,12 @@
-const logBase = require('tracer')
-let log = require('tracer').colorConsole()
+const log = require('tracer').colorConsole()
 
 module.exports = function (logConfig = {}, logProcess) {
     logConfig = logConfig || {}
     return function xauth(ctx, next) {
-        if (logConfig.level) {
-            log = logBase.colorConsole({ level: logConfig.level })
-        }
         log.info(ctx.method, ctx.header.host + ctx.url)
-        log.info('BODY', ctx.request.body)
+        if (ctx.method != 'GET') {
+            log.info('BODY', ctx.request.body)
+        }
         if (logConfig.header && logConfig.header.loglist && logConfig.header.loglist.length > 0) {
             for (let item of logConfig.header.loglist) {
                 log.info('HEADER.' + item, ctx.request.header[item])
