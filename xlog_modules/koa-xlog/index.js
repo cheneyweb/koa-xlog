32d7907b4d6b1621efb1b9a7b1622a92e04539f3
keep_moving <meibin_whu@163.com>
2021-08-06 12:32:22 +0800

    feat:优化日志，一个访问只打印一行

--- xlog_modules/koa-xlog/index.js
+++ xlog_modules/koa-xlog/index.js
@@ -1,22 +1,40 @@
-const log = require('tracer').console()
+const log = require("tracer").console();
 
 module.exports = function (logConfig = {}, logProcess) {
-    logConfig = logConfig || {}
-    return function xlog(ctx, next) {
-        log.info(ctx.method, `${ctx.header.host}${ctx.url}`)
-        if (ctx.method != 'GET') {
-            log.info('BODY', ctx.request.body)
-        }
-        if (logConfig.header && logConfig.header.loglist && logConfig.header.loglist.length > 0) {
-            for (let item of logConfig.header.loglist) {
-                log.info('HEADER.' + item, ctx.request.header[item])
-            }
-        } else if (logConfig.header) {
-            log.info('HEADER', ctx.request.header)
-        }
-        if (logProcess) {
-            logProcess(ctx)
-        }
-        return next()
+  logConfig = logConfig || {};
+
+  return function xlog(ctx, next) {
+
+    // 日志内容
+    
+    const logData = {
+      BODY: undefined,
+      header: undefined,
+    };
+
+    if (ctx.method != "GET") {
+      logData.BODY = ctx.request.body;
     }
-}
+
+    if (
+      logConfig.header &&
+      logConfig.header.loglist &&
+      logConfig.header.loglist.length > 0
+    ) {
+      logData.header = {};
+      for (let item of logConfig.header.loglist) {
+        logData.header[item] = ctx.request.header[item];
+      }
+    } else if (logConfig.header) {
+      logData.header = ctx.request.header;
+    }
+
+    log.info(ctx.method, `${ctx.header.host}${ctx.url}`, logData);
+
+    if (logProcess) {
+      logProcess(ctx);
+    }
+
+    return next();
+  };
+};
